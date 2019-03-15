d9a96f18dd4d63d3147b1c3b769420c8d55a3a02
JSMontoya <juansmm@outlook.com>
2019-03-15 07:42:00 -0500

    added empty basic modules and dependencies

--- 4.UI/OG.Zoo.UI/ClientApp/src/app/app-routing.module.ts
+++ 4.UI/OG.Zoo.UI/ClientApp/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'security', loadChildren: './security/security.module#SecurityModule' },
+  { path: 'params', loadChildren: './params/params.module#ParamsModule' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
