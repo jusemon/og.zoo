8513efcf3b21df2e3fb61f46bb4951028e3f20c0
JSMontoya <juansmm@outlook.com>
2019-04-04 11:36:16 -0500

    Added IE compatibility in GET request

--- 4.UI/OG.Zoo.UI/ClientApp/src/app/shared/services/interceptor.service.ts
+++ 4.UI/OG.Zoo.UI/ClientApp/src/app/shared/services/interceptor.service.ts
@@ -10,15 +10,22 @@ import { Router } from '@angular/router';
 export class InterceptorService implements HttpInterceptor {
   constructor(private authService: AuthService, private snackBar: MatSnackBar, private router: Router) { }
 
-    intercept(req: HttpRequest < any >, next: HttpHandler): Observable < HttpEvent < any >> {
-      return next.handle(req).pipe(
-        catchError((error: HttpErrorResponse) => {
-          if (error.status === 401) {
-            this.authService.deauthenticate();
-            this.router.navigate(['/auth']);
-            this.snackBar.open('Auth denied.');
-            return throwError('Auth denied');
-          }
-        }));
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let custom = req.clone();
+    if (req.method === 'GET') {
+      custom = req.clone({
+        headers: req.headers.set('Cache-Control', 'no-cache').set('Pragma', 'no-cache')
+      });
     }
+
+    return next.handle(custom).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          this.authService.deauthenticate();
+          this.router.navigate(['/auth']);
+          this.snackBar.open('Auth denied.');
+          return throwError('Auth denied');
+        }
+      }));
   }
+}
